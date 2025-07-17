12eb2295769c8e9d8792c073b298a6d01408a6d1
snowjam02 <snowjam02@naver.com>
2025-07-17 18:12:45 +0900

    Title: 함수를 이용한 369 게임, Time: 95ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 250717/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,23 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+
+def get_three(num):
+    if num == 0:
+        return False 
+    else:
+        if (num % 10) % 3 == 0:
+            return True 
+        else:
+            return get_three(num // 10) 
+
+cnt = 0
+for i in range(a, b + 1):
+    if i % 3 == 0:
+        cnt += 1 
+    else:
+        if get_three(i) == True:
+            cnt += 1
+
+print(cnt)
+
