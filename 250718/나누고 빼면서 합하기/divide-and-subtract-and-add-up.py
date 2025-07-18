415e018692a0c4882d6a146572617e19c2b94d6b
snowjam02 <snowjam02@naver.com>
2025-07-18 13:34:52 +0900

    Title: 나누고 빼면서 합하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250718/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 250718/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -4,13 +4,13 @@ arr = list(map(int, input().split()))
 
 tot = 0
 while M != 1: 
+    tot += arr[M - 1]
+  
     if M % 2 != 0:
         M -= 1 
     else:
         M = int( M / 2 )
-
-    tot += arr[M]
-tot += arr[M]
+tot += arr[0]
 print(tot)
 
     
