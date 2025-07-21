b7fbccfd49935437019c60236efd8fbc39c131dd
snowjam02 <snowjam02@naver.com>
2025-07-21 10:49:48 +0900

    Title: 국영수 순이지, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250721/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,12 @@
+N = int(input()) 
+
+arr = []
+for i in range(N):
+    a, b, c, d = map(str, input().split()) 
+    arr.append((a, int(b), int(c), int(d)))
+
+arr.sort(key= lambda x: (-x[1],-x[2],-x[3]))
+
+
+for i in arr:
+    print(i[0], i[1], i[2], i[3])
