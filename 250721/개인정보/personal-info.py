f9fb7806268a98ad0fbc95bdad17ab82dbb9769a
snowjam02 <snowjam02@naver.com>
2025-07-21 14:07:10 +0900

    Title: 개인정보, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250721/개인정보/personal-info.py
@@ -0,0 +1,13 @@
+arr = []
+
+for i in range(5):
+    name, h, w = map(str, input().split()) 
+    arr.append((name,int(h), float(w)))
+
+print("name")
+arr.sort(key=lambda x:x[0])
+
+for i in arr:
+    print(i[0], i[1], i[2])
+
+    
