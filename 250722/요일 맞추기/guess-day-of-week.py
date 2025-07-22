3247db5bd4896bb017320bf95a345b6f8f0f010a
snowjam02 <snowjam02@naver.com>
2025-07-22 10:53:51 +0900

    Title: 요일 맞추기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250722/요일 맞추기/guess-day-of-week.py
+++ 250722/요일 맞추기/guess-day-of-week.py
@@ -1,27 +1,39 @@
 m1,d1,m2,d2 = map(int, input().split()) 
 
-sum_d = 0
-for i in range(m1, m2):
+sum_d1 = 0
+for i in range(1,m1):
     if i == 1 or i == 3 or i == 5 or i == 7 or i == 8 or i == 10 or i ==12:
-        sum_d += 31 
+        sum_d1 += 31 
     elif i == 2:
-        sum_d += 28 
+        sum_d1 += 28 
     else:
-        sum_d += 30 
+        sum_d1 += 30 
+sum_d1 += d1 
 
-sum_d = (sum_d - d1 + d2) % 7
+sum_d2 = 0
+for i in range(1,m2):
+    if i == 1 or i == 3 or i == 5 or i == 7 or i == 8 or i == 10 or i ==12:
+        sum_d2 += 31 
+    elif i == 2:
+        sum_d2 += 28 
+    else:
+        sum_d2 += 30 
+sum_d2 += d2 
 
-if sum_d == 0:
+diff = (sum_d2 - sum_d1) % 7
+
+
+if diff == 0:
     print("Mon") 
-elif sum_d == 1:
+elif diff == 1:
     print("Tue") 
-elif sum_d == 2:
+elif diff == 2:
     print("Wed") 
-elif sum_d == 3:
+elif diff == 3:
     print("Thu") 
-elif sum_d == 4:
+elif diff == 4:
     print("Fri") 
-elif sum_d == 5:
+elif diff == 5:
     print("Sat") 
-elif sum_d == 6:
+elif diff == 6:
     print("Sun") 
