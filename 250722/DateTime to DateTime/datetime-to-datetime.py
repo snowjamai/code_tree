2a4273a6f19bd8acea016603a5d416684bd52a54
snowjam02 <snowjam02@naver.com>
2025-07-22 10:45:49 +0900

    Title: DateTime to DateTime, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250722/DateTime to DateTime/datetime-to-datetime.py
+++ 250722/DateTime to DateTime/datetime-to-datetime.py
@@ -3,7 +3,7 @@ A, B, C = map(int, input().split())
 base_m = 10 * 24 * 60 + 11 * 60 + 11 
 
 target_m = (A - 1) * 24 * 60 + B * 60 + C 
-if target_m < 0:
+if target_m - base_m < 0:
     print("-1")
 else:
     print(target_m - base_m)
