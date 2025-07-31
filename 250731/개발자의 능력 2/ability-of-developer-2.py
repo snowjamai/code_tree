aa66f01fdf1cf76b82f3f446c93c323c6b8428e9
snowjam02 <snowjam02@naver.com>
2025-07-31 16:46:46 +0900

    Title: 개발자의 능력 2, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250731/개발자의 능력 2/ability-of-developer-2.py
+++ 250731/개발자의 능력 2/ability-of-developer-2.py
@@ -5,18 +5,10 @@ arr.sort()
 re1 = arr[0] + arr[-1]
 re2 = arr[1] + arr[-2]
 re3 = arr[2] + arr[-3]
+re = []
+re.append(re1)
+re.append(re2)
+re.append(re3)
 
-if re1 < re2:
-    tmp = re1 
-    re1 = re2 
-    re2 = tmp 
-if re1 < re3:
-    tmp = re3 
-    re3 =re1
-    re1 = tmp 
-if re2 < re3:
-    tmp = re2
-    re2 = re3 
-    re3 = tmp 
-
-print(re1 - re3 )
+re.sort()
+print(re[2] - re[0] )
