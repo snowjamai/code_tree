bc68f4cc3c082beb6b6789d632e009ea1299d34e
snowjam02 <snowjam02@naver.com>
2025-07-31 14:11:55 +0900

    Title: 바구니 안의 사탕 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250731/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250731/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,19 +1,18 @@
 N, K = map(int, input().split())
 
-arr = [0] * 101 
+arr = [0] * 101
+
 
 for i in range(N):
     a, b = map(int, input().split())
-
-    arr[b] = a
+    arr[b] += a 
 
 
 max_v = 0
-for i in range(K, 101- K + 1):
-    tmp = arr[i - K:i + K + 1]
+for i in range(K, 101 - K):
+    tmp = arr[i - K: i + K + 1]
     sum_v = sum(tmp)
     if max_v < sum_v:
-       
         max_v = sum_v
 
 print(max_v)
