c76525f1a75f8d471633a85c831c7e73a5db00aa
snowjam02 <snowjam02@naver.com>
2025-08-01 14:34:43 +0900

    Title: 스승의 은혜 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250801/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,31 @@
+N, B = map(int, input().split())
+
+arr = []
+for i in range(N):
+    a = int(input())
+    arr.append(a)
+
+
+
+max_cnt  = 0
+for i in range(len(arr)):
+    target_cost = arr[i] / 2 
+    tmp = arr[:i] + arr[i + 1:] + [target_cost]
+    tmp.sort()
+
+    sum_v = 0
+    cnt = 0
+
+    for j in tmp:
+        sum_v += j
+
+        if sum_v > B:
+            if max_cnt < cnt:
+                max_cnt = cnt 
+            break
+        else:
+            cnt += 1 
+
+print(max_cnt)
+
+
