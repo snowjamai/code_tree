b96c1ebc844928ca6e10caa2b001ca0812ad2d21
snowjam02 <snowjam02@naver.com>
2025-08-04 18:46:37 +0900

    Title: 팀으로 하는 틱택토 2, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250804/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250804/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -3,6 +3,8 @@ board = [list(map(int, input())) for _ in range(3)]
 dx, dy = [1,0,1,1], [0,1,1,-1] 
 
 cnt = 0
+tmp = {}
+
 for h in range(3):
     for w in range(3):
         for i in range(4):
@@ -16,7 +18,11 @@ for h in range(3):
                 second = board[h + dy[i]][w + dx[i]]
                 third = board[h + dy[i] * 2][w + dx[i] * 2]
                 if first == second and first != third or first == third and first != second or second == third and first != second:
-
-                    cnt += 1
+                    arr = [first, second, third]
+                    arr.sort() 
+                    if (arr[0], arr[-1]) not in tmp:
+                        tmp[(arr[0], arr[-1])] = 1 
+                    
+                        cnt += 1
 
 print(cnt)
