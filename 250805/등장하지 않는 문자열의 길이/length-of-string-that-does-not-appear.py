94068709d87a7f57c5e02b9be6a5e92ca930b29a
snowjam02 <snowjam02@naver.com>
2025-08-05 08:39:21 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250805/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 250805/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -5,9 +5,9 @@ min_len = N
 
 for i in range(1, N + 1):
     is_dupli = False 
-    for j in range(N - i):
+    for j in range(N):
         con_str = s[j:j + i]
-        left = s[j + i:]
+        left = s[j + 1:]
         if con_str in left:
             is_dupli = True 
     if is_dupli == False:
