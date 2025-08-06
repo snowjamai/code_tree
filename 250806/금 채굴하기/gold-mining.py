311b645d3560ebbbf138536d70c9a9d2540b1c7c
snowjam02 <snowjam02@naver.com>
2025-08-06 14:55:24 +0900

    Title: 금 채굴하기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250806/금 채굴하기/gold-mining.py
@@ -0,0 +1,36 @@
+N, M =map(int, input().split())
+
+
+board = [list(map(int, input().split())) for _ in range(N)]
+
+def cnt_gold(x,y, k):
+    cnt = 0
+    for h in range(y - k, y + k + 1):
+        for w in range(x - k, x + k + 1):
+            if abs(w - x) + abs(h - y) > k:
+                continue 
+            else:
+                if h < 0 or h >= N or w < 0 or w >= N:
+                    continue 
+                if board[h][w] == 1:
+                    cnt += 1
+    return cnt 
+
+max_cnt = 0
+max_cost = 0
+for h in range(N):
+    for w in range(N):
+        for k in range(1, N):
+            cnt = cnt_gold(w,h,k)
+            cost = cnt * M 
+            f_cost = cost - k * k - (k + 1) * (k + 1)
+            if f_cost < 0:
+                continue
+            else:
+                if max_cnt < cnt:
+                    # max_cost = f_cost
+                    max_cnt = cnt 
+
+print(max_cnt) 
+
+
