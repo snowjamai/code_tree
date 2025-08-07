e26158a7cf86c2aa21acb1257c60933953b58faf
snowjam02 <snowjam02@naver.com>
2025-08-07 17:19:58 +0900

    Title: 컨베이어 벨트, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250807/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,16 @@
+N, T = map(int, input().split())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+b = b[::-1]
+for i in range(T):
+    tmp = a[2]
+    a = b[:1] + a[:2]
+    b = b[1:] + [tmp] 
+b = b[::-1]
+
+for i in a:
+    print(i, end = ' ')
+print("")
+for i in b:
+    print(i, end = ' ')
