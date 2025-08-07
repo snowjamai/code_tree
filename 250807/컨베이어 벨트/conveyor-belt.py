e391344ddf4d0a7bea5da43b7821f44516b1f683
snowjam02 <snowjam02@naver.com>
2025-08-07 17:21:09 +0900

    Title: 컨베이어 벨트, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250807/컨베이어 벨트/conveyor-belt.py
+++ 250807/컨베이어 벨트/conveyor-belt.py
@@ -4,8 +4,8 @@ a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 b = b[::-1]
 for i in range(T):
-    tmp = a[2]
-    a = b[:1] + a[:2]
+    tmp = a[N - 1]
+    a = b[:1] + a[:N - 1]
     b = b[1:] + [tmp] 
 b = b[::-1]
 
