656616537ec4125680d43ed2e89bc8216b0f5fe5
snowjam02 <snowjam02@naver.com>
2025-08-08 14:41:49 +0900

    Title: 최단 Run Length 인코딩, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250808/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,28 @@
+a = input()
+# len_a = len(a)
+
+
+def get_encoding(num):
+    dict_num = []
+
+    for i in num:
+        if dict_num == []:
+            dict_num.append((i, 1))
+        else:
+            if dict_num[-1][0] == i:
+                dict_num[-1] = (i, dict_num[-1][1] + 1)
+            else:
+                dict_num.append((i,1))
+
+    return dict_num 
+
+min_len = float('inf')
+for i in range(len(a)):
+    arr = a[-i:] + a[:-i]
+    tmp = get_encoding(arr)
+    lt = len(tmp)
+    if lt < min_len:
+        min_len = lt
+
+
+print(min_len * 2)
