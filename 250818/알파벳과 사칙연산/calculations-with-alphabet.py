25d45e6120607152eb57c784c904c2052e4b550d
snowjam02 <snowjam02@naver.com>
2025-08-18 15:05:50 +0900

    Title: 알파벳과 사칙연산, Time: 76ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250818/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 250818/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -191,37 +191,35 @@ elif 'b' in s:
 
 elif 'a' in s:
     mapper = {
-        "a":a,
-        "b":b,
-        "c":c,
-        "d":d,
-        "e":e,
-        "f":f
+        "a":a
     }
     re = 0 
-    for i in range(len(s)):
-        if s[i] == '-':
-            if i == 1:
-                prev = mapper[s[i - 1]]
-            else:
-                prev = re
-            forw = mapper[s[i + 1]]
-            re = prev - forw
-        elif s[i] == '*':
-            if i == 1:
-                prev = mapper[s[i - 1]]
-            else:
-                prev = re
-            forw = mapper[s[i + 1]]
-            re = prev * forw
-        elif s[i] == '+':
-            if i == 1:
-                prev = mapper[s[i - 1]]
-            else:
-                prev = re
-            forw = mapper[s[i + 1]]
-            re = prev + forw
-    if max_v < re:
-        max_v = re 
-
+    if len(s) != 1:
+        for a in range(1,5):
+            for i in range(len(s)):
+                if s[i] == '-':
+                    if i == 1:
+                        prev = mapper[s[i - 1]]
+                    else:
+                        prev = re
+                    forw = mapper[s[i + 1]]
+                    re = prev - forw
+                elif s[i] == '*':
+                    if i == 1:
+                        prev = mapper[s[i - 1]]
+                    else:
+                        prev = re
+                    forw = mapper[s[i + 1]]
+                    re = prev * forw
+                elif s[i] == '+':
+                    if i == 1:
+                        prev = mapper[s[i - 1]]
+                    else:
+                        prev = re
+                    forw = mapper[s[i + 1]]
+                    re = prev + forw
+            if max_v < re:
+                max_v = re 
+    else:
+        max_v = 4
 print(max_v)
