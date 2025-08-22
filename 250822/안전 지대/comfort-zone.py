c1f112f7b1cb6d766fc1a79cc35ad037c8c10f4d
snowjam02 <snowjam02@naver.com>
2025-08-22 15:58:09 +0900

    Title: 안전 지대, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250822/안전 지대/comfort-zone.py
+++ 250822/안전 지대/comfort-zone.py
@@ -23,23 +23,39 @@ def dfs(x, y, v):
 
 max_v = 0
 max_k = 0 
-
-for k in range(1, 101):
-    tmp = copy.deepcopy(board)
-    visited = [[0] * M for _ in range(N)]
-    v = 0
+def eliminate(k):
     for h in range(N):
         for w in range(M):
             if tmp[h][w] <= k:
                 tmp[h][w] = 0
-            if tmp[h][w] != 0:
+
+def dfs(x, y):
+    dx, dy = [1,-1,0,0],[0,0,1,-1]
+    for i in range(4):
+        nx, ny = dx[i] + x  , dy[i] + y 
+        if nx < 0 or nx >= M or ny < 0 or ny >= N:
+            continue 
+        else:
+            if visited[ny][nx] == 0 and tmp[ny][nx] != 0:
+                visited[ny][nx] = 1 
+                dfs(nx,ny)
+for k in range(1, 101):
+    tmp = copy.deepcopy(board)
+    visited = [[0] * M for _ in range(N)]
+    v = 0
+    eliminate(k)
+    for h in range(N):
+        for w in range(M):
+            if tmp[h][w] != 0 and visited[h][w] == 0:
+                visited[h][w] = 1
                 v += 1
-                dfs(w, h, v)
+                dfs(w, h)
+    
                
     if v > max_v:
         max_v = v 
         max_k = k 
-print(max_k)
+print(max_k, max_v)
 
 
             
