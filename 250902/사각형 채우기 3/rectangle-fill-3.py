0da85a16ab20929f2a12e93fbe98bf930a704c4e
snowjam02 <snowjam02@naver.com>
2025-09-02 11:11:57 +0900

    Title: 사각형 채우기 3, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250902/사각형 채우기 3/rectangle-fill-3.py
+++ 250902/사각형 채우기 3/rectangle-fill-3.py
@@ -10,6 +10,6 @@ for i in range(N + 1):
     if i <= 3: 
         continue 
 
-    dp[i] = (dp[i - 1] * 2 + dp[i - 2] ) % 1000000007
+    dp[i] = (dp[i - 1] * 2 + dp[i - 2] ) 
 
-print(dp[N] )
+print(dp[N] % 1000000007)
