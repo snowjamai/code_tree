67ab76971d78f361248df2f9a4438a076ae4e177
snowjam02 <snowjam02@naver.com>
2025-09-02 11:11:00 +0900

    Title: 사각형 채우기 3, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250902/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,15 @@
+N = int(input()) 
+
+dp = [0] * 1001 
+
+dp[1] = 2
+dp[2] = 7
+dp[3] = 16
+
+for i in range(N + 1):
+    if i <= 3: 
+        continue 
+
+    dp[i] = (dp[i - 1] * 2 + dp[i - 2] ) % 1000000007
+
+print(dp[N] )
